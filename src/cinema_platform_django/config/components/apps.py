ee596aed154e9a77d3666e6133b5203889305e5b
shamshetov2007 <azshamshetov@mai.education>
2026-04-06 20:02:23 +0300

    Handler for user service is done + also it was tested

--- src/cinema_platform_django/config/components/apps.py
+++ src/cinema_platform_django/config/components/apps.py
@@ -5,6 +5,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    'user_service',
+    "rest_framework",
+    "user_service",
 ]
