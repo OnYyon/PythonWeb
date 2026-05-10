ef26883626adb5e91d4d1f8118caa8bcb9328771
Kim Oleg <YAoleg2007@ya.ru>
2026-05-10 13:40:41 +0300

    refactor: change path models to infrastructure.models

---
+++ src/cinema_platform_django/subscription/infrastructure/models/__init__.py
@@ -0,0 +1,8 @@
+from src.cinema_platform_django.subscription.infrastructure.models.plans import (
+    PlanModel,
+)
+from src.cinema_platform_django.subscription.infrastructure.models.subscription import (
+    SubscriptionModel,
+)
+
+__all__ = ["PlanModel", "SubscriptionModel"]
