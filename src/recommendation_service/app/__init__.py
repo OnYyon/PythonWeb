1d8768e17373bdb2bb92ed8191949d95722cddc4
Pavel-d6 <dudinpavel12@gmail.com>
2026-05-24 23:54:35 +0300

    Feat/recommendation service (#46)
    Co-authored-by: Kim Oleg <101497214+OnYyon@users.noreply.github.com>
    Co-authored-by: Дудин Павел Александрович <239235282+Pavel-d6@users.noreply.github.com>
    Co-authored-by: shamshetov2007 <azshamshetov@mai.education>
    Co-authored-by: sospx <151164407+sospx@users.noreply.github.com>
    Co-authored-by: lormy <Lormy7@yandex.ru>
    Co-authored-by: Lormy7 <157532744+Lormy7@users.noreply.github.com>

--- src/recommendation_service/app/__init__.py
+++ src/recommendation_service/app/__init__.py
@@ -7,8 +7,9 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "postgresql+psycopg://cinema:P%40ssw0rd@localhost:5432/cinema"
+        "postgresql+psycopg://cinema:P%40ssw0rd@db:5432/cinema"
     )
+
     app.config["SQLALCHEMY_ECHO"] = False
 
     db.init_app(app)
